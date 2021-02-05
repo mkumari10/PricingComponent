f85a503e3d40dbdcfd13c00d45cbe85433cfa35a
Manju <74829190+mkumari10@users.noreply.github.com>
2021-02-05 18:17:51 +0530

    Add events

--- interactive-pricing-component-main/index.js
+++ interactive-pricing-component-main/index.js
@@ -1,14 +1,57 @@
 $('document').ready(function(e){
-	
+ 
+       const discount = () =>
+       {  
+          let rangevalue = $('input[type="range"]').val();
+          let num = parseInt($('input[type="range"]').val());
+          let givedist = $('input[type="checkbox"]');
+          if(rangevalue <= 10)
+          {   
+              rangevalue = num*10;
+              $('#views').html(`${rangevalue}k`);
+              if($(givedist).is(':checked'))
+              {
+                $('.price').html(`$${(12*(num+7)*0.75)}`);
+                $('.month').html('/ year');
+              }else{
+                $('.price').html(`$${num+7}`);
+                $('.month').html('/ month');
+              }
+          }else if(rangevalue < 19)
+          {
+              rangevalue = ((num%10)*100)+100;
+              $('#views').html(`${rangevalue}k`);
+              if($(givedist).is(':checked'))
+              {
+                $('.price').html(`$${(Math.floor(num*1.9)*12*0.75)}`);
+                $('.month').html('/ year');
+              }else{
+                $('.price').html(`$${Math.floor(num*1.9)}`);
+                $('.month').html('/ month');
+              }
+          }else
+          {   
+             $('#views').html(`1M`); 
+              if($(givedist).is(':checked'))
+              {
+                $('.price').html(`$${(Math.floor(num*1.9)*12*0.75)}`);
+                $('.month').html('/ year');
+              }else{
+                $('.price').html(`$${Math.floor(num*1.9)}`);
+                $('.month').html('/ month');
+              }   
+          }
+
+       }
+
 	$(document).on('input change','#slider',function(e){
-       $('.price').html(`$${$(this).val()*4}`);
-       // $('#views').html(`${i++}k`)
-       console.log($(this).val());
+	   discount();
 	});
+
+       $('input[type="checkbox"]').on('click',function(e){
+            discount();
+       });
+
 });
 
-// - 10K pageviews / $8 per month 
-// - 50K pageviews / $12 per month 
-// - 100K pageviews / $16 per month
-// - 500k pageviews / $24 per month
-// - 1M pageviews / $36 per month
+
